f621f3a19c6ec128483b636071445149eea83488
armnutthapon <armnutthapon@hotmail.co.th>
2021-02-07 16:13:21 +0700

    Create Main.vue

---
+++ src/layouts/Main.vue
@@ -0,0 +1,318 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <div class="text-white toolbarT">
+        <q-toolbar class="row full-height justify-center">
+          <q-btn flat>
+            <q-icon name="laberu" />
+            <q-toolbar-title class="titleName"
+              ><strong>LABERU.AI</strong>
+            </q-toolbar-title>
+          </q-btn>
+          <q-space />
+          <q-btn
+            flat
+            round
+            dense
+            icon="search"
+            class="q-mr-xs text-blue-grey-7"
+          />
+          <q-btn flat round dense icon="group_add" class="text-blue-grey-7" />
+        </q-toolbar>
+      </div>
+    </q-header>
+    <q-parallax height="720">
+      <div class="area">
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </q-parallax>
+
+    <q-page-container style="padding-top: 0">
+      <div class="context center">
+        <div class="row justify-around">
+          <div class="col-lg-3">
+            <q-card flat bordered class="my-card">
+              <q-card-section>
+                <div class="text-h6 text-center"><b>Image#01</b></div>
+              </q-card-section>
+              <q-card-section>
+                <div class="q-pa-md">
+                <img src="./images/test.jpg" alt="" height="300px">
+                </div>
+              </q-card-section>
+              <q-separator inset />
+
+              <q-card-section>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="col-lg-4 center">
+            <q-card flat bordered class="my-card">
+              <q-card-section>
+                <div class="text-h4 text-center"><b>Describe the Image</b></div>
+              </q-card-section>
+              <q-card-section>
+                <div class="q-pa-md">
+                  <div class="q-gutter-md" style="max-width: 400px">
+                    <q-input outlined v-model="text" label="E-mail" />
+                    <q-input outlined v-model="text" label="Password" />
+                  </div>
+                </div>
+              </q-card-section>
+              <q-separator inset />
+
+              <q-card-section>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+      </div>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from "components/EssentialLink.vue";
+
+export default {
+  data() {
+    return {
+      text: "",
+    };
+  },
+};
+</script>
+
+<style>
+.toolbarT {
+  height: 80px;
+  background: #f8f8f8;
+}
+
+.titleName {
+  color: #666877;
+}
+
+.my-card {
+  width: 400px;
+  border-radius: 10px;
+}
+
+.context {
+  width: 100%;
+  position: absolute;
+  top: 10rem;
+}
+
+.area {
+  background: #e6e8ea;
+  background: -webkit-linear-gradient(to left, #8f94fb, #4e54c8);
+  width: 100%;
+  height: 100vh;
+}
+
+.circles {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.circles li {
+  position: absolute;
+  display: block;
+  list-style: none;
+  width: 20px;
+  height: 20px;
+  background: rgba(94, 51, 51, 0.24);
+  animation: animate 25s linear infinite;
+  bottom: -150px;
+}
+
+.circles li:nth-child(1) {
+  left: 25%;
+  width: 80px;
+  height: 80px;
+  animation-delay: 0s;
+}
+
+.circles li:nth-child(2) {
+  left: 10%;
+  width: 20px;
+  height: 20px;
+  animation-delay: 2s;
+  animation-duration: 12s;
+}
+
+.circles li:nth-child(3) {
+  left: 70%;
+  width: 20px;
+  height: 20px;
+  animation-delay: 4s;
+}
+
+.circles li:nth-child(4) {
+  left: 40%;
+  width: 60px;
+  height: 60px;
+  animation-delay: 0s;
+  animation-duration: 18s;
+}
+
+.circles li:nth-child(5) {
+  left: 65%;
+  width: 20px;
+  height: 20px;
+  animation-delay: 0s;
+}
+
+.circles li:nth-child(6) {
+  left: 75%;
+  width: 110px;
+  height: 110px;
+  animation-delay: 3s;
+}
+
+.circles li:nth-child(7) {
+  left: 35%;
+  width: 150px;
+  height: 150px;
+  animation-delay: 7s;
+}
+
+.circles li:nth-child(8) {
+  left: 50%;
+  width: 25px;
+  height: 25px;
+  animation-delay: 15s;
+  animation-duration: 45s;
+}
+
+.circles li:nth-child(9) {
+  left: 20%;
+  width: 15px;
+  height: 15px;
+  animation-delay: 2s;
+  animation-duration: 35s;
+}
+
+.circles li:nth-child(10) {
+  left: 85%;
+  width: 150px;
+  height: 150px;
+  animation-delay: 0s;
+  animation-duration: 11s;
+}
+
+@keyframes animate {
+  0% {
+    transform: translateY(0) rotate(0deg);
+    opacity: 1;
+    border-radius: 0;
+  }
+
+  100% {
+    transform: translateY(-1000px) rotate(720deg);
+    opacity: 0;
+    border-radius: 50%;
+  }
+}
+
+.iconic {
+  /* Group 65 */
+
+  position: absolute;
+  width: 30px;
+  height: 30px;
+
+  box-shadow: inset 0px 0px 62px rgba(0, 0, 0, 0.25);
+
+  /* Ellipse 74 */
+
+  position: absolute;
+  width: 30px;
+  height: 30px;
+
+  background: #6d6b8a;
+
+  /* Group 64 */
+
+  position: absolute;
+  width: 16.62px;
+  height: 19.56px;
+
+  /* Rectangle 128 */
+
+  position: absolute;
+  width: 1.8px;
+  height: 15.21px;
+
+  background: #d15eff;
+  border-radius: 3px;
+
+  /* Rectangle 130 */
+
+  position: absolute;
+  width: 1.8px;
+  height: 10.37px;
+
+  background: #d15eff;
+  border-radius: 3px;
+
+  /* Rectangle 132 */
+
+  position: absolute;
+  width: 1.8px;
+  height: 6.57px;
+
+  background: #d15eff;
+  border-radius: 3px;
+
+  /* Rectangle 129 */
+
+  position: absolute;
+  width: 1.8px;
+  height: 5.65px;
+
+  background: #d15eff;
+  border-radius: 3px;
+  transform: rotate(-90deg);
+
+  /* Rectangle 131 */
+
+  position: absolute;
+  width: 2.18px;
+  height: 16.62px;
+
+  background: #d15eff;
+  border-radius: 3px;
+  transform: rotate(-90deg);
+
+  /* Rectangle 133 */
+
+  position: absolute;
+  width: 1.8px;
+  height: 16.62px;
+
+  background: #d15eff;
+  border-radius: 3px;
+  transform: rotate(-90deg);
+}
+</style>
